e0282a7bd802edb67a8795e4cf9be435663542b3
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2022-08-31 18:10:55 +0200

    Spline
    -added script and prefab for spline

---
+++ Assets/_Prefabs/Prefab_RoadSpline/Scripts/Line.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line : MonoBehaviour
+{
+    public Vector3 p0, p1;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
